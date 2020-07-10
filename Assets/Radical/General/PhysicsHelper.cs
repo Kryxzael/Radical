68589b57af5e046324f1d238aac107dc25d55e09
Kryxzael <kasper.ramskjell98@gmail.com>
2020-07-11 00:27:36 +0200

    Implement jumping

---
+++ Assets/Radical/General/PhysicsHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Radical.General
+{
+    /// <summary>
+    /// Helper functions for physics objects
+    /// </summary>
+    public static class PhysicsHelper
+    {
+        /// <summary>
+        /// Checks whether the object is grounded
+        /// </summary>
+        /// <param name="body"></param>
+        /// <returns></returns>
+        public static bool OnGround(this Collider collider)
+        {
+            if (collider == null)
+                return false;
+
+            Vector3[] pnts = new[]
+            {
+                new Vector3(collider.bounds.min.x, collider.bounds.min.y + 0.01f, collider.bounds.min.z),
+                new Vector3(collider.bounds.min.x, collider.bounds.min.y + 0.01f, collider.bounds.max.z),
+                new Vector3(collider.bounds.max.x, collider.bounds.min.y + 0.01f, collider.bounds.min.z),
+                new Vector3(collider.bounds.max.x, collider.bounds.min.y + 0.01f, collider.bounds.max.z),
+                new Vector3(collider.bounds.center.x, collider.bounds.min.y + 0.01f, collider.bounds.center.z)
+            };
+
+
+            return pnts.Any(i => Physics.RaycastAll(new Ray(i, Vector3.down), 0.1f).Any());
+        }
+    }
+}
--- Assets/Radical/Player/PlayerController.cs
+++ Assets/Radical/Player/PlayerController.cs
@@ -1,73 +1,84 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Assets.Radical.General;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace Assets.Radical.Player
 {
-    /*
-     * Input 
-     */
-    private const string MOVE_AXIS_HORIZONTAL = "Horizontal";
-    private const string MOVE_AXIS_VERTICAL = "Vertical";
-
-    private Rigidbody _rigidbody;
-    private SpriteAnimator _animator;
-    private SpriteRenderer _renderer;
-
-    [Header("Movement")]
-    public float MoveSpeed;
-
-    [Header("Animations")]
-    public SpriteAnimation IdleAnimation;
-    public SpriteAnimation WalkAnimation;
-
-
-    private void Awake()
+    public class PlayerController : MonoBehaviour
     {
-        _rigidbody = GetComponent<Rigidbody>();
-        _animator = GetComponent<SpriteAnimator>();
-        _renderer = GetComponent<SpriteRenderer>();
-    }
+        /*
+         * Input 
+         */
+        private const string MOVE_AXIS_HORIZONTAL = "Horizontal";
+        private const string MOVE_AXIS_VERTICAL = "Vertical";
 
-    private void Update()
-    {
-        HandleMoveInput();
-        UpdateAnimation();
-    }
+        private Rigidbody _rigidbody;
+        private Collider _collider;
+        private SpriteAnimator _animator;
+        private SpriteRenderer _renderer;
 
-    /// <summary>
-    /// Performs movement logic
-    /// </summary>
-    private void HandleMoveInput()
-    {
-        //Horizontal movement
-        _rigidbody.velocity = new Vector3(
-            x: Input.GetAxisRaw(MOVE_AXIS_HORIZONTAL) * MoveSpeed,
-            y: _rigidbody.velocity.y,
-            z: Input.GetAxisRaw(MOVE_AXIS_VERTICAL) * MoveSpeed
-        );
+        [Header("Movement")]
+        public float MoveSpeed;
+        public float JumpHeight;
 
-        Debug.Log(_rigidbody.velocity);
-    }
+        [Header("Animations")]
+        public SpriteAnimation IdleAnimation;
+        public SpriteAnimation WalkAnimation;
 
-    /// <summary>
-    /// Updates the player's current animation
-    /// </summary>
-    private void UpdateAnimation()
-    {
-        //Direction
-        if (_rigidbody.velocity.x > 0)
-            _renderer.flipX = false;
 
-        else if (_rigidbody.velocity.x < 0)
-            _renderer.flipX = true;
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _collider = GetComponent<Collider>();
+            _animator = GetComponent<SpriteAnimator>();
+            _renderer = GetComponent<SpriteRenderer>();
+        }
 
-        //Animation
-        if (Mathf.Abs(_rigidbody.velocity.x) > 0 || Mathf.Abs(_rigidbody.velocity.z) > 0)
-            _animator.Animation = WalkAnimation;
+        private void Update()
+        {
+            HandleMoveInput();
+            UpdateAnimation();
+        }
 
-        else
-            _animator.Animation = IdleAnimation;
+        /// <summary>
+        /// Performs movement logic
+        /// </summary>
+        private void HandleMoveInput()
+        {
+            //Horizontal movement
+            _rigidbody.velocity = new Vector3(
+                x: Input.GetAxisRaw(MOVE_AXIS_HORIZONTAL) * MoveSpeed,
+                y: _rigidbody.velocity.y,
+                z: Input.GetAxisRaw(MOVE_AXIS_VERTICAL) * MoveSpeed
+            );
+
+            //Jump
+            if (_collider.OnGround() && Input.GetButtonDown("Jump"))
+                _rigidbody.velocity += Vector3.up * JumpHeight;
+        }
+
+        /// <summary>
+        /// Updates the player's current animation
+        /// </summary>
+        private void UpdateAnimation()
+        {
+            //Direction
+            if (_rigidbody.velocity.x > 0)
+                _renderer.flipX = false;
+
+            else if (_rigidbody.velocity.x < 0)
+                _renderer.flipX = true;
+
+
+
+            //Animation
+            if (Mathf.Abs(_rigidbody.velocity.x) > 0 || Mathf.Abs(_rigidbody.velocity.z) > 0)
+                _animator.Animation = WalkAnimation;
+
+            else
+                _animator.Animation = IdleAnimation;
+        }
     }
 }
