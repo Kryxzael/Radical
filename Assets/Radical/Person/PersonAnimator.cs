6187bf2c67989d06f6a38ed930bc2482827ef954
Kryxzael <kasper.ramskjell98@gmail.com>
2020-07-12 03:40:41 +0200

    Add final(-ish?) enemy sprites

---
+++ Assets/Radical/Person/PersonAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Radical.General;
+using Assets.Radical.Triggerable;
+using UnityEngine;
+
+namespace Assets.Radical.Person
+{
+    [RequireComponent(typeof(DirectionManager))]
+    public class PersonAnimator : TriggerableComponent
+    {
+        private DirectionManager _directionManager;
+
+        public TwosidedSpriteAnimation IdleAnimation;
+        public TwosidedSpriteAnimation StunAnimation;
+        public TwosidedSpriteAnimation ChaseAnimation;
+
+        private void Awake()
+        {
+            _directionManager = GetComponent<DirectionManager>();
+            _directionManager.Animation = IdleAnimation;
+        }
+
+        public override void OnTriggered()
+        {
+            base.OnTriggered();
+            _directionManager.Animation = ChaseAnimation;
+        }
+    }
+}
